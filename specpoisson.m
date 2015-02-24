ee9724be4f5573b2dc63883f39e09934ab519cba
Kevin Diomedi <kpd237@vt.edu>
2015-02-24 16:57:23 -0500

    Spectral Domain solver
    Using code from hw1 for the spectral solver. Review field for periodic
    boundary condition enforcement on field calculation.

---
+++ specpoisson.m
@@ -0,0 +1,19 @@
+function phi = specpoisson(rho,dx)
+
+eps0=1;
+%Take fft and reorder.
+rhok=fft(rho,pow2(nextpow2(length(rho))));
+rhok=fftshift(rhok);
+
+N=length(rhok);
+n=linspace(-N/2,N/2-1,N);
+
+%Calculate Kn^2
+kn2=pi*n/N;
+Kn2i=(dx/2./sin(kn2*dx)).^2;
+
+phik=rhok./eps0.*Kn2i;%calc charge by dividing by Kn^2
+phik=ifftshift(phik)/(N*pi);%put fft back in correct order, scale
+phik(1)=0%2*pi/(dx*N);%eliminate inf from divison by zero.
+phi=ifft(phik);
+end 
