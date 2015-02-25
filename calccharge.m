0f1bbeba3006f5cc7c81e994125c750c71c7bc74
Kevin Diomedi <kpd237@vt.edu>
2015-02-25 13:51:13 -0500

    Calc charge corrections for arbitrary length/grid
    Modified indexing/ calculations in calcCharge to account for the fact
    that we have an abitrary sized domain with non-unity spatial grid.

--- calccharge.m
+++ calccharge.m
@@ -5,15 +5,21 @@ function charge =calccharge(particle,qmr,dt,dx,lx,np,wp,ng)
 charge=zeros(ng,1);
 
 for i=1:size(particle,1);
-	%Find grid above and below particle
-	belind=floor(particle(i,1))+1;%bel(ow)ind(ex)
-	abind=mod(belind,ng)+1;	 %ab(ove)ind(ex)
-	%since we have 1 indexing, belind is actually the x value of the grid point above it.
-	charge(belind)=charge(belind)+(belind-particle(i,1));
-	charge(abind) =charge(abind) +(particle(i,1)-(belind-1));
-
+	%Find grid above and below particle using zero indexing.
+	% this allows us to multiply by dx to get the x value at the grid point
+	% also allow the use of mod() to account for periodicity.
+	bel_grid=floor(particle(i,1)/dx);%grid number below (for 1 indexing need to add 1)/
+	%bel_ind will always be between 0 and ng-1
+	abov_grid=bel_grid+1;%index 1 above this, we enforce periodicity
+		% by taking mod below in the index.
+	
+	%weight charge to grid, recall bel/abov_grid is 0 indexing to allow dx multi
+	%	add 1 for indexing matlab matrices.
+	charge(bel_grid+1)=charge(bel_grid+1)+(dx*abov_grid-particle(i,1));
+	charge(mod(abov_grid,ng)+1) =charge(mod(abov_grid,ng)+1) +(particle(i,1)-dx*bel_grid);
+% note here we have imposed periodicity in charge index here.
 end
 %multiply by charge of particle
-charge=charge*wp^2/np/qmr/dx;
+charge=charge*wp^2/np/qmr/dx/lx;
 
 end
