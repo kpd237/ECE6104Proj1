5fd05420fa789eece2a3184744094e9cfda5dd12
Kevin Diomedi <kpd237@vt.edu>
2015-02-25 13:52:17 -0500

    efield grid corrections for abitrary length
    Previous length was coupled to number of grid cells
    
    Also have better understanding of grid cell periodicity that was likely
    a bug in previous code. Last "grid point" is really the same one as the
    first. So we should wrap around here, I don't think I was accounting for
    this and was treating it as a separate point. Fixed now.

--- efield.m
+++ efield.m
@@ -1,19 +1,9 @@
 function [E,phi]=efield(charge,dx,ng,lx,eps0,E)
-spectral=true;
-%==============
 %FFT charge
-if spectral
-	phi=specpoisson(charge,dx);
-else
-%==========
-%bc at 1st only
-	bc=-2*diag(ones(ng-1,1))+diag(ones(lx-1,1),-1)+diag(ones(lx-1,1),1);
-	phi=zeros(ng,1);
-	phi(2:ng)=bc\charge(2:ng);
-	phi=-phi/eps0*dx^2;
-end
+phi=specpoisson(charge,dx);
 %FD electric field
-E(2:lx)=-(phi(3:ng)-phi(1:lx-1))/(2*dx);
+
+E(2:ng-1)=-(phi(3:ng)-phi(1:ng-2))/(2*dx);
 E(1)=-(phi(2)-phi(ng))/(2*dx);
-E(ng)=-(phi(1)-phi(lx))/(2*dx);
+E(ng)=-(phi(1)-phi(ng-1))/(2*dx);
 end
