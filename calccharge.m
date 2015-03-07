5343f87cd0cdd6950a5a011c3335fe2a8379048f
Kevin Diomedi <kpd237@vt.edu>
2015-03-06 21:22:45 -0500

    ops, forgot to accumulate charge, was setting.

--- calccharge.m
+++ calccharge.m
@@ -5,19 +5,19 @@ function charge =calccharge(particle,qmr,dt,dx,lx,np,wp,ng)
 charge=zeros(ng,1);
 
 for i=1:size(particle,1);
-	xi=particle(i,1);
-	jm1=floor(xi/dx);%multiply by dx for x_(j-1)
+	xi=particle(i,1)
+	jm1=floor(xi/dx)%multiply by dx for x_(j-1)
 	%add 1 : (jm1+1) multiply by dx for x_(j)
 	
 	%determine index
-	bel=mod(jm1,ng)+1;%must add 1 for because 1 indexing.
-	abo=mod(jm1+1,ng)+1;
+	bel=mod(jm1,ng)+1%must add 1 for because 1 indexing.
+	abo=mod(jm1+1,ng)+1
 	
 
 %weight below p_(j-1)=(xj-xi)
-	charge(bel)=((jm1+1)*dx-xi);
+	charge(bel)=charge(bel)+((jm1+1)*dx-xi);
 %weight above p_(j)=(xi-x(j-1))
-	charge(abo)=(xi-jm1*dx);
+	charge(abo)=charge(abo)+(xi-jm1*dx);
 end
 %multiply by charge of particle
 charge=charge*wp^2/np/qmr/dx/lx;
