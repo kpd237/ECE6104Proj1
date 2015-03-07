6693951018bf829504bf4ea0c34fe297ac958c91
Kevin Diomedi <kpd237@vt.edu>
2015-03-07 14:35:08 -0500

    Specpoisson, matching fortran code.

--- specpoisson.m
+++ specpoisson.m
@@ -1,19 +1,37 @@
 function phi = specpoisson(rho,dx)
 
 eps0=1;
+
+N=length(rho);
+ng2=N/2;
+for k=1:ng2
+        kdx2=(pi/N)*k;
+        ksqi(k)=+eps0/((2.0*sin(kdx2)/dx).^2);
+end
+
+rho(1)=0;
+%rhok(N+1)=0;%not sure on this one)
+%hdx=0.5*dx;
+
+%rho=rho*hdx;
 %Take fft and reorder.
-rhok=fft(rho);
+rhok=fft(rho,pow2(nextpow2(length(rho))));
 rhok=fftshift(rhok);
+rhok(1)=0;
 
-N=length(rhok);
-n=linspace(-N/2,N/2-1,N).';
+phik(1)=0;
+for k=2:ng2
+	kk=N+2-k;
+	phik(k)=ksqi(k-1)*rhok(k);
+	phik(kk)=ksqi(k-1)*rhok(kk);
+	phik(ng2+1)=ksqi(ng2)*rhok(ng2+1);
+end
 
-%Calculate Kn^2
-kn2=pi*n/N;
-Kn2i=(dx/2./sin(kn2*dx)).^2;
+%li=1.0/l;
+%rhok=rhok*li;
+%phik=phik*li;
 
-phik=rhok./eps0.*Kn2i;%calc charge by dividing by Kn^2
-phik=ifftshift(phik)/(N*pi);%put fft back in correct order, scale
-phik(1)=0;%2*pi/(dx*N);%eliminate inf from divison by zero.
+phik=ifftshift(rhok);
 phi=ifft(phik);
-end 
+end
+
