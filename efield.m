0bdd569e804ab9ce6539fbfe1eb3492dc8b292cd
Kevin Diomedi <kpd237@vt.edu>
2015-03-08 16:17:26 -0400

    Validation of efield/spectral solver.

--- efield.m
+++ efield.m
@@ -3,7 +3,11 @@ function [E,phi]=efield(charge,dx,ng,lx,eps0,E)
 phi=specpoisson(charge,dx);
 %FD electric field
 
-E(2:ng-1)=-(phi(3:ng)-phi(1:ng-2))/(2*dx);
-E(1)=-(phi(2)-phi(ng))/(2*dx);
-E(ng)=-(phi(1)-phi(ng-1))/(2*dx);
+E(2:ng-1)=-(phi(3:ng)-phi(1:ng-2));
+E(1)=-(phi(2)-phi(ng));
+E(ng)=-(phi(1)-phi(ng-1));
+%E(1)=0;
+%E(2)=0;
+E=E/(2*dx);
+
 end
---
+++ validate_Efield.m
@@ -0,0 +1,47 @@
+%=====================================
+% efield - FD Electric field solver
+%=====================================
+ng=512;
+L=2;
+dx=L/ng;
+x=dx*(0:ng-1);
+eps0=1;
+E=zeros(ng,1);
+
+% case 1, phi=sin(pi);
+charge=sin(pi*x)*pi^2;%This charge gives desired phi with spectral solver.
+%this will give potential = sin(x);
+[E,phi]=efield(charge,dx,ng,L,eps0,E);
+h=figure(1);
+plot(x,phi,x,charge/pi^2);
+legend('\phi','charge');
+%expected E
+Eexp=-pi*cos(pi*x);
+h=figure(2);
+plot(x,E,x,Eexp);
+legend('FFT','Expected');
+title('Validation for \phi=sin(\pi*x)');
+xlabel('x (m)');
+ylabel('V/m');
+saveas(h,'validate_FD_1.epsc2');
+
+% case 2
+N=512;
+x=linspace(2*pi/N,2*pi,N);
+dx=x(2)-x(1);
+charge=-(sin(x).^2-cos(x)).*exp(cos(x));
+phi_ana=exp(cos(x));
+[E,phi]=efield(charge,dx,ng,L,eps0,E);
+C=phi(1)-phi_ana(1);
+figure(1);
+Eexp=sin(x).*exp(cos(x));
+plot(x,phi,x,phi_ana+C);
+legend('\phi','expected');
+figure(2);
+plot(x,E,x,Eexp);
+legend('FFT','Expected');
+title('Validation for \phi=exp(cos(x))');
+xlabel('x (m)');
+ylabel('V/m');
+saveas(h,'validate_FD_2.epsc2');
+
